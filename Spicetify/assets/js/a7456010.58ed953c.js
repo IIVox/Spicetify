1a36c6bd38cae247d0c0512beb3242b3cb559397
Vox <104469999+IIVox@users.noreply.github.com>
2024-09-27 13:07:07 +0100

    Delete Spicetify directory

--- Spicetify/assets/js/a7456010.58ed953c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkspicetify_docs=self.webpackChunkspicetify_docs||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
